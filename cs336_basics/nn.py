00df531d62a3aa17b52f3697bd9c61a96cce32b8
Pasicall <zd030510@163.com>
2026-08-28 20:27:55 +0800

    添加Embedding

--- cs336_basics/nn.py
+++ cs336_basics/nn.py
@@ -17,3 +17,15 @@ class Linear(nn.Module):
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return torch.einsum('...i, oi -> ...o', x, self.weight)
+
+class Embedding(nn.Module):
+    def __init__(self, num_embeddings, embedding_dim, device=None, dtype=None):
+        super().__init__()
+        factory_kwargs = {'device': device, 'dtype': dtype}
+
+        self.weight = nn.Parameter(torch.empty((num_embeddings,embedding_dim),**factory_kwargs))        
+        std = 1.0
+        nn.init.trunc_normal_(self.weight,mean=0.0,std=1.0,a=-3.0,b=3.0)
+
+    def forward(self,token_ids:torch.Tensor) -> torch.Tensor:
+        return self.weight[token_ids]
